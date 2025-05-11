26e94cb15f1ba10b0f823e140617faa63d07c843
lyakhovay <lyahov.aleksey3@wb.ru>
2025-05-11 18:03:40 +0300

    implement slice and pointer fields

--- homework/generics_and_reflection/homework_test.go
+++ homework/generics_and_reflection/homework_test.go
@@ -12,10 +12,12 @@ import (
 // go test -v homework_test.go
 
 type Person struct {
-	Name    string `properties:"name"`
-	Address string `properties:"address,omitempty"`
-	Age     int    `properties:"age"`
-	Married bool   `properties:"married"`
+	Name      string   `properties:"name"`
+	Address   string   `properties:"address,omitempty"`
+	Age       int      `properties:"age"`
+	Married   bool     `properties:"married"`
+	Phone     *string  `properties:"phone,omitempty"`
+	AltPhones []string `properties:"alt_phones,omitempty"`
 }
 
 func Serialize(data any) string {
@@ -32,10 +34,15 @@ func Serialize(data any) string {
 		if !ok {
 			continue
 		}
-		fieldToWrite := fieldToString(tag, v.Field(i))
-		if len(fieldToWrite) > 0 {
-			result = append(result, fieldToWrite)
+		newTag, omitempty := getPropertyTag(tag)
+		if omitempty && v.Field(i).IsZero() {
+			continue
 		}
+		str := strings.Builder{}
+		str.WriteString(newTag)
+		str.WriteString("=")
+		str.WriteString(fieldToString(v.Field(i)))
+		result = append(result, str.String())
 	}
 	return strings.Join(result, "\n")
 }
@@ -54,32 +61,33 @@ func getPropertyTag(tag string) (string, bool) {
 	return strings.Join(newTag, ","), omitempty
 }
 
-func fieldToString(tag string, field reflect.Value) string {
-	newTag, omitempty := getPropertyTag(tag)
-	if omitempty && field.IsZero() {
-		return ""
-	}
-	str := strings.Builder{}
-	str.WriteString(newTag)
-	str.WriteString("=")
+func fieldToString(field reflect.Value) string {
 	switch field.Kind() {
+	case reflect.Pointer:
+		return fieldToString(field.Elem())
+	case reflect.Slice, reflect.Array:
+		result := make([]string, 0, field.Len())
+		for i := 0; i < field.Len(); i++ {
+			result = append(result, fieldToString(field.Index(i)))
+		}
+		return strings.Join(result, ",")
 	case reflect.String:
-		str.WriteString(field.String())
+		return field.String()
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		str.WriteString(strconv.FormatInt(field.Int(), 10))
+		return strconv.FormatInt(field.Int(), 10)
 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-		str.WriteString(strconv.FormatUint(field.Uint(), 10))
+		return strconv.FormatUint(field.Uint(), 10)
 	case reflect.Float32, reflect.Float64:
-		str.WriteString(strconv.FormatFloat(field.Float(), 'g', -1, 64))
+		return strconv.FormatFloat(field.Float(), 'g', -1, 64)
 	case reflect.Bool:
-		str.WriteString(strconv.FormatBool(field.Bool()))
+		return strconv.FormatBool(field.Bool())
 	default:
 		panic("unsupported field type")
 	}
-	return str.String()
 }
 
 func TestSerialization(t *testing.T) {
+	phone := "+7(999)1234567"
 	tests := map[string]struct {
 		person Person
 		result string
@@ -103,6 +111,26 @@ func TestSerialization(t *testing.T) {
 				Address: "Paris",
 			},
 			result: "name=John Doe\naddress=Paris\nage=30\nmarried=true",
+		},
+		"test case with pointer field": {
+			person: Person{
+				Name:    "John Doe",
+				Age:     30,
+				Married: true,
+				Address: "Paris",
+				Phone:   &phone,
+			},
+			result: "name=John Doe\naddress=Paris\nage=30\nmarried=true\nphone=+7(999)1234567",
+		},
+		"test case with slice field": {
+			person: Person{
+				Name:      "John Doe",
+				Age:       30,
+				Married:   true,
+				Address:   "Paris",
+				AltPhones: []string{"+7(999)7654321", "+7(000)1234567"},
+			},
+			result: "name=John Doe\naddress=Paris\nage=30\nmarried=true\nalt_phones=+7(999)7654321,+7(000)1234567",
 		},
 	}
 
